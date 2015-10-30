c27472e104224bd7768962c3c9385729390314bb
LouisEugene <leugene@stanford.edu>
2015-10-29 21:40:13 -0700

    Initial Commit

---
+++ CS147demo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CS147demo
+//
+//  Created by Louis Eugene on 29/10/2015.
+//  Copyright © 2015 Louis Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CS147demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CS147demo
+//
+//  Created by Louis Eugene on 29/10/2015.
+//  Copyright © 2015 Louis Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
